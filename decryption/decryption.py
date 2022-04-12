a470996d7ee993cbe12624ab3783a6e37a9da2ac
Max Leyman <maximleyman@yandex.ru>
2022-04-12 19:57:11 +0700

    [+] added subprocess logic

--- decryption/decryption.py
+++ decryption/decryption.py
@@ -1,9 +1,18 @@
 import asyncio
-from asyncio import get_event_loop, sleep
+import dataclasses
+from asyncio import get_event_loop, sleep, Task
+from asyncio.subprocess import create_subprocess_shell, PIPE
 from concurrent.futures import ProcessPoolExecutor
 from .queue import RedisQueue
 from .db_clients import SQLiteClient
 from .task import TaskStatus, Task
+# ToDo: rework imports
+
+
+@dataclasses.dataclass
+class _DecryptionResult:
+    success: bool
+    result: str
 
 
 class DecryptionTaskManager:
@@ -44,24 +53,22 @@ class Decrypter:
         self._task_manager = task_manager
         self._executor = ProcessPoolExecutor(max_workers=2)
         self._decryption_task = self._event_loop.create_task(self._run_decryption_loop())
+        self._running_tasks: list[Task] = []
 
     async def _run_decryption_loop(self):
         while True:
             try:
-                if (task := await self._task_manager.get_next_task()) is not None:
+                await self._check_running_tasks()
+                if len(self._running_tasks) < 2 and (task := await self._task_manager.get_next_task()) is not None:
                     print(task.path_file)
-                    executor = ProcessPoolExecutor()
-                    # ToDO: Need to try to use asyncio-subprocess
+                    self._event_loop.create_task(self._run_john(task))
 
-                    await get_event_loop().run_in_executor(executor, self._decrypt, self, task.path_file)
-                    await sleep(5)
-
-                    # await self._event_loop.run_in_executor(self._executor, self._decrypt, (file_path,))
                 print("Here")
 
                 await sleep(5)
             except asyncio.CancelledError:
                 print('exit task')
+                self._running_tasks = []
                 break
             except Exception as e:
                 context = {'message': 'Error with decryption process',
@@ -69,11 +76,32 @@ class Decrypter:
                            'task': self._decryption_task}
                 self._event_loop.call_exception_handler(context)
 
+    async def _check_running_tasks(self):
+        for task in self._running_tasks:
+            if not task.done():
+                continue
+
+            result = task.result()
+
+    async def _handle_done_task(self, task):
+        await self._task_manager.set_task_status(task.task_id, TaskStatus.FINISHED)
+
+    async def _run_john(self, task: Task) -> _DecryptionResult:
+        process = await create_subprocess_shell('ls /home', stdout=PIPE, stderr=PIPE)
+        stdout, stderr = await process.communicate()
+        if stdout:
+            print(f'[stdout]\n{stdout.decode()}')
+        if stderr:
+            print(f'[stderr]\n{stderr.decode()}')
+            # ToDo: parse john response
+
+        await sleep(3)
+
     async def stop(self):
         if self._decryption_task is not None and not self._decryption_task.cancelled():
             self._decryption_task.cancel()
 
     def _decrypt(self, task: Task):
         print(f"Decrypt file {task.path_file}")
-        await self._task_manager.set_task_status(task.task_id, TaskStatus.FINISHED)
+        # await self._task_manager.set_task_status(task.task_id, TaskStatus.FINISHED)
         # self._task_manager.handle_task(file_path)
